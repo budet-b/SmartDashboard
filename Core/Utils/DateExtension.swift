b9f1242711fe1b01b72ea87bf0c05cc4d67c6444
Alexandre Toubiana <alextoub@hotmail.com>
2018-12-04 12:16:18 +0100

    Get weather of current day

---
+++ Core/Utils/DateExtension.swift
@@ -0,0 +1,9 @@
+//
+//  DateExtension.swift
+//  SmartDashboard
+//
+//  Created by Alexandre Toubiana on 12/4/18.
+//  Copyright © 2018 Benjamin Budet. All rights reserved.
+//
+
+import Foundation
